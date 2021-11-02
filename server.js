012ec250e395a9efa2b48e1fb7f527c91b6bb5a0
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-11-02 19:05:24 -0400

    server.js upgrades

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -20,6 +20,17 @@ mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+// Connects either to MongoDB Atlas if on heroku site or to the local, if running locally --//
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/budgetdb',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   },
+// );
 
 // routes
 app.use(require("./routes/api.js"));
